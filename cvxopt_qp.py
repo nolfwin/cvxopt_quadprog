c444cca08cb0402790206f88fac8e52d953a18c1
Nolfwin <nontawat.mk@gmail.com>
2018-03-11 22:12:26 +0900

    init commit.

---
+++ cvxopt_qp.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cvxopt
+
+
+def quadprog(H, f, L, k, Aeq, beq, lb, ub):
+    """
+    input: Numpy arrays, the format follows MATLAB quadprog function: https://www.mathworks.com/help/optim/ug/quadprog.html
+    Output: Numpy array of the solution
+    """
+    n_var = H.shape[1]
+
+    P = cvxopt.matrix(H)
+    q = cvxopt.matrix(f)
+
+    if L is not None or k is not None:
+        assert(k is not None and L is not None)
+        if lb is not None:
+            L = np.vstack([L, -np.eye(n_var)])
+            k = np.vstack([k, -lb])
+
+        if ub is not None:
+            L = np.vstack([L, np.eye(n_var)])
+            k = np.vstack([k, ub])
+
+        L = cvxopt.matrix(L)
+        k = cvxopt.matrix(k)
+
+    if Aeq is not None or beq is not None:
+        assert(Aeq is not None and beq is not None)
+        Aeq = cvxopt.matrix(Aeq)
+        beq = cvxopt.matrix(beq)
+
+    sol = cvxopt.solvers.qp(P, q, L, k, Aeq, beq)
+
+    return np.array(sol['x'])
